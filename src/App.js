1974a7c23dd73023f35d0d1121b7fa6c9fb80192
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2020-09-30 21:23:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Nav from './components/Nav/Nav'
 import './App.scss'
 
 const App = () => {
     return (
-        <main>Hello World</main>
+        <main>
+            <Router>
+                <Route path='/' component={Nav} />
+            </Router>
+        </main>
+
     )
 }
 
