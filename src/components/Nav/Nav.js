699f345d263f43439b9302098545ed66cacd63f6
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2020-09-30 21:23:13 -0500

    Nav Component
    Created a Side Nav component to rederect throught the web app. (with styling)
    Created CardNavLink to display the individual links. (with styling)
    Added a blurred backgorung effect to nav
    Made global scss variables.

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Svgs from '../../data/svg'
+import CardNavLink from '../Cards/CardNavLink/CardNavLink'
+import './Nav.scss'
+
+const Nav = (props) => {
+    const path = props.history.location.pathname
+    return(
+        <nav className={`nav`}>
+            <div className={`nav__container`} />
+                <div className={`nav__main`}>
+                    <div className={`nav__search`}>
+                    </div>
+                    <div className={`nav__links`}>
+                        <div className={`nav__manage`}>
+                            <CardNavLink to='/' svg={Svgs.Lock} name='Passwords' isActive={path === '/'} />
+                            <CardNavLink to='/password-generator' svg={Svgs.LockAdd} name='Password Generator' isActive={path === '/password-generator'} />
+                        </div>
+                        <div className={`nav__tools`}>
+                            <CardNavLink to='/import' svg={Svgs.Import} name='Import' isActive={path === '/import'} />
+                            <CardNavLink to='/export' svg={Svgs.Export} name='Export' isActive={path === '/export'} />
+                            <CardNavLink to='/settings' svg={Svgs.Settings} name='Settings' isActive={path === '/settings'} />
+                        </div>
+                    </div>
+                </div>
+                <div className={`nav__foot`}>
+                    <CardNavLink to='/about' name='About' isActive={path === '/about'} />
+                    <CardNavLink to='/privacy' name='Privacy' isActive={path === '/privacy'} />
+                </div>
+        </nav>
+    )
+}
+
+export default Nav
