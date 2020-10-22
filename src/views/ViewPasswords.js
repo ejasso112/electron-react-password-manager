b57a0812eb44ada7fb9900f712e26b15afe7a792
Eduardo Jasso <49826641+ejasso112@users.noreply.github.com>
2020-10-22 04:36:42 -0500

    ViewPasswords SortBy Update
    Created a SortBy select to sort passwords by name/category/email

--- src/views/ViewPasswords.js
+++ src/views/ViewPasswords.js
@@ -13,10 +13,51 @@ import { PassVaultContext } from '../contexts/PassVaultContext'
 import ImportSvg from '../res/ImportSvg'
 import '../styles/ViewPasswords.scss'
 
+
+
 const ViewPasswords = (props) => {
+    let [sortBy, SetSortBy] = React.useState('name')
     const contextData = React.useContext(PassVaultContext)
     console.log(contextData.passwords)
     
+    const compareByName = (a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    }
+
+    const compareByCategory = (a, b) => {
+        if (a.category + a.name < b.category + b.name) {
+            return -1;
+        }
+        if (a.category + a.name > b.category + b.name) {
+            return 1;
+        }
+        return 0;
+    }
+
+    const compareByEmail = (a, b) => {
+        if (a.email + a.name < b.email + b.name) {
+            return -1;
+        }
+        if (a.email + a.name > b.email + b.name) {
+            return 1;
+        }
+        return 0;
+    }
+
+    console.log(sortBy)
+    const passwordsByName = [...contextData.passwords.sort(compareByName)]
+    const passwordsByCategory = [...contextData.passwords.sort(compareByCategory)]
+    const passwordsByEmail = [...contextData.passwords.sort(compareByEmail)]
+
+    const sortedPasswords = sortBy === 'name' ? passwordsByName :
+    sortBy === 'category' ? passwordsByCategory : passwordsByEmail
+
     return (
         <article className={`viewPasswords view`}>
             <div className={`viewPasswords__menu`}>
@@ -26,15 +67,20 @@ const ViewPasswords = (props) => {
                     <ExportFile><PasswordsButton svg={ImportSvg.Export} name='Export' /></ExportFile>
                 </div>
                 <div className={`viewPasswords__menu__group`}>
+                    <select style={{height: 42, background: 'transparent', border: 0, outline: 0}} onChange={e => SetSortBy(e.target.value)}>
+                        <option value="name">Name</option>
+                        <option value="category">Category</option>
+                        <option value="email">Email</option>
+                    </select>
                     <PasswordsButtonView svg={ImportSvg.GridView} view='grid' />
                     <PasswordsButtonView svg={ImportSvg.DetailsView} view='list' />
                 </div>
             </div>
             {contextData.view === 'grid' &&
-                <PasswordsViewGrid data={contextData.passwords} activeId={props.match.params.id} />
+                <PasswordsViewGrid data={sortedPasswords} activeId={props.match.params.id} />
             }
             {contextData.view === 'list' &&
-                <PasswordsViewList data={contextData.passwords} activeId={props.match.params.id} />
+                <PasswordsViewList data={sortedPasswords} activeId={props.match.params.id} />
             }
         </article>
     )
